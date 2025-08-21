1201ad82d98092e44de1016c11aa6b28416dc6aa
Ahmed Khaled <ahmed@gmail.com>
2025-08-22 00:53:09 +0300

    update companies slider

--- src/app/components/companies/companies.component.ts
+++ src/app/components/companies/companies.component.ts
@@ -1,11 +1,63 @@
-import { Component } from '@angular/core';
+
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { CarouselModule, OwlOptions } from 'ngx-owl-carousel-o';
 
 @Component({
   selector: 'app-companies',
-  imports: [],
+  standalone: true,                       // ✅ IMPORTANT
+  imports: [CommonModule, CarouselModule],// ✅ Now these imports take effect
+  
   templateUrl: './companies.component.html',
-  styleUrl: './companies.component.css'
+  styleUrls: ['./companies.component.css'] // ✅ plural
+  // ❌ remove schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
-export class CompaniesComponent {
+export class CompaniesComponent implements OnInit {
+
+  slidesStore:any[]=[]
+
+
+  customOptions: OwlOptions = {
+    loop: true,
+    margin: 10,
+    nav: false,
+    dots: false,
+    autoplay: true,
+    autoplayTimeout: 1000,
+    navSpeed:700,
+    autoplaySpeed: 800, // 0.8s smooth animation
+    navText:['',''],
+    autoplayHoverPause: true,
+    responsive: {
+      0:   { items: 2 },
+      400: { items: 3 },
+      740: { items: 4 },
+      940: { items: 6 }
+    }
+  };
+
+
+ngOnInit(): void {
+  
+
+  this.slidesStore=[
+
+
+
+    {id:"1",src:"company1.png"},
+    {id:"2",src:"company2.png"},
+    {id:"3",src:"company3.png"},
+    {id:"4",src:"company4.png"},
+    {id:"5",src:"company5.png"},
+    {id:"6",src:"company6.png"},
+
+
+
+
+  ]
+
+
+}
+
 
 }
--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,23 @@
 
 // src/main.ts
 import { bootstrapApplication } from '@angular/platform-browser';
-import { provideRouter } from '@angular/router';
+import { RouterModule, provideRouter } from '@angular/router';
 import { AppComponent } from './app/app.component';
 import { routes } from './app/app.routes';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { LoaderService } from './app/services/loader.service';
 import { loaderInterceptor } from './app/interceptors/loader.interceptor';
+import { importProvidersFrom } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 
-const loaderService = new LoaderService();
 
 bootstrapApplication(AppComponent, {
   providers: [
     provideRouter(routes),
-    provideHttpClient(withInterceptors([loaderInterceptor]))
+    provideHttpClient(withInterceptors([loaderInterceptor])),
+    importProvidersFrom(RouterModule,BrowserAnimationsModule)
+    
+  
   ]
 });
